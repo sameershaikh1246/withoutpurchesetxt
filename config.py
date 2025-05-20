ddda10bc9efd7c62e957c5ddc4f7bbecd6f7195a
SAMEER JI <srrahman07498@gmail.com>
2025-05-20 17:44:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 api_id = "23274330"
 api_hash = "970e2e79779707c56d2b453b3a6eea48"
 bot_token = "7558941479:AAEgEYtLznREuwSJIwrYYCpqmvN9E58X-JM"
-auth_users = [7991495621]
+auth_users = [7431004177]
